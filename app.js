0a3cb7c2b122182d326ac282438345ba4fcdfc08
David Wang <dwang163@my.bcit.ca>
2020-02-11 16:22:20 -0800

    javascript alert fixed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // This is a change
+// Fixed alert
 // Add DOM selectors to target input and UL movie list
 var inp = document.querySelector("input");
 var myMovieList = document.querySelector("ul");
